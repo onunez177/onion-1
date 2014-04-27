6ce97b6e4343cfca260c5dbc0d1e4543cb57b2b4
Markus Karileet <markuskarileet@hotmail.com>
2014-04-27 20:57:56 +0300

    Added new translations

--- i18n/en.php
+++ i18n/en.php
@@ -6,17 +6,23 @@
  * @var array
  */
 $lang = array (
-	'title' => 'Sommelier',
+	'choose' => 'Pick Your poison, friend!',
+	'drinkType1' => 'Beer',
+	'drinkType2' => 'Wine',
+	'planet' => 'Planet',
+	'welcomeTo' => 'Welcome to',	
+	'introtext' => '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>',
+	/* menu items */
 	'home' => 'Home',
 	'reviews' => 'Reviews',
 	'products' => 'Products',
 	'newreview' => 'New review',
 	'newproduct' => 'New Product',
-	'links' => 'Links',
+	'links' => 'Links',	
+	'aboutSite' => 'About this site',
 		
-	'contactInfo' => 'Contact Info',
-	'contactDetails' => 'In case of bugs and/or suggestions, please contact me at:<br><br>E-mail: markuskarileet@hotmail.com<br>',	
-		
+	/* product adding */
 	'type' => 'Type',
 	'subtype' => 'Subtype',
 	'manufactor' => 'Manufactor',
@@ -27,12 +33,13 @@ $lang = array (
 	'user' => 'User',
 	'cancel' => 'Cancel',
 	'save' => 'Save',
+	'allProducts' => 'List of all products',
 	'latestProducts' => 'Latest Products',
 	'noProducts' => 'No products found',
 	'productReviews' => 'Product reviews',
 	'averageRating' => 'Average rating',
     'addReview' => 'Add review',
-	
+	'topFresh' => 'Top Fresh',
 	'product' => 'Product',
 	'color' => 'Appearance',
 	'smell' => 'Aroma',
@@ -41,8 +48,11 @@ $lang = array (
 	'rating' => 'Rating',
 	'notInList' => 'Product not in list?',
 	'latestReviews' => 'Latest Reviews',
+	'allReviews' => 'List of all reviews',
 	'picture' => 'Image (PNG)',
 		
+	
+	/* Product types and subtypes */
 	'beer' => 'Beer',
 	'wine' => 'Wine',
 	'lager' => 'Lager',
@@ -56,7 +66,15 @@ $lang = array (
 	'soda' => 'Soda',
 	'rootbeer' => 'Root beer',
 	
-		
+	'aboutus' => 'About us',
+	'aboutusDetails' => '<p>I am software architect/developer at Tieto Estonia AS who also enjoys a occasional beer or two.</p><p>I created this site to store all my findings in a newbish way.</p>',
+	'help' => 'Help',
+	'helpDetails' => '<p>&raquo; To enter review, go to <b>New Review</b> page and find the product You just tasted</p><p>&raquo; If You do not find the product from our list, go to <b>New Product</b> page and add it Yourself!',
+	'contactInfo' => 'Contact Info',
+	'contactDetails' => '<p>If You have any suggestions, bug reports or any other ideas, please do not hesitate to contact me on following address</p><p>markuskarileet@hotmail.com</p>',	
+	'downloadDisclaimer' => 'This template  downloaded form',	
+	
+	/* technical messages */
 	'insertSuccess' => 'Insertion successful',
 	'insertFail' => 'Insert failed for some reason',
 	'internalError' => 'Internal error occurred, event has been logged and worked with',
