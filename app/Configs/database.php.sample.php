35ce84b1903f7f2f58bd9ede39a0f6d6a81ff0e9
Markus Karileet <markuskarileet@hotmail.com>
2014-04-30 14:47:43 +0300

    added config placeholder example files

---
+++ app/Configs/database.php.sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File holds database credentials. Set them according to Your system.
+ */
+
+/**
+ * Database configuration array
+ * @var array
+ */
+$db = array(
+	'host' => 'SERVER',
+	'user' => 'USERNAME',
+	'pass' => 'PASSWORD',		
+	'schema' => 'DATABASE NAME'		
+);
---
+++ app/Configs/misc.php.sample.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File for misc configurations. Add Your own config here also!
+ */
+
+/**
+ * Conf variable whether to set names to utf8 or not. Set to true if non-latin
+ * characters break when selecting from database!
+ * @var boolean
+ */
+define('SETDBENCODING', true);
+
+/**
+ * Name of the image that is used as a default image when nothing is uploaded
+ * Path to this image is found in paths.php (IMAGES)
+ * @var var
+ */
+define('NOIMAGE', 'noimage.png');
+
+/**
+ * URL to page that users can like on Facebook
+ * @var var
+ */
+define('LIKEURL', '127.0.0.1');
