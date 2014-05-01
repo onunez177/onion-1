811514d4d96e47543bd8e54790886a828f7f9779
Markus Karileet <markuskarileet@hotmail.com>
2014-05-01 21:26:39 +0300

    Added estonian translation

---
+++ i18n/et.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Variable holds english translations for the site. Translations are accessed
+ * by array key. Translation is keys value. This file needs to be included in
+ * \Views\Page::_loadLanguage() method
+ * @var array
+ */
+$lang = array (
+	'choose' => 'Milline on Su lemmikmürk, mu sõber?',
+	'drinkType1' => 'Õlu',
+	'drinkType2' => 'Vein',
+	'planet' => 'Planet',
+	'welcomeTo' => 'Teretulemast lehele',	
+	'introtext' => '<p>Lehekülg sai loodud mõttega kirja panna erinevate jookide maitsed, 
+		mida ma alates 2014 aasta esimesest poolest proovinud olen. Lisaks on teine põhjus ka: soovisin proovida enda
+		tehtud lihtsat ORMi (Object Relational Mapping). Niisiis, siin see on: lehekülg mis on tehtud kasutades
+		Onion ORMi ( https://github.com/Shmarkus/onion )! Kindlasti vaadake lähtekoodi ning kasutage seda ka oma 
+		lehekülgedel..</p><p>..või</p><p>..kui Sul on mõnda hiljutist õlle/veini maitsmist olnud, siis pane see
+		siia kirja! Kui seda toodet veel ei ole, saab selle lisada uue toote menüü alt</p><p> Seda kõike SAAKS ka
+		ratebeer.com lehel teha, kuid SEE oleks liiga meinstriim ;)</p>',
+	/* menu items */
+	'home' => 'Avaleht',
+	'reviews' => 'Ülevaated',
+	'products' => 'Tooted',
+	'newreview' => 'Uus ülevaade',
+	'newproduct' => 'Uus toode',
+	'links' => 'Lingid',	
+	'aboutSite' => 'Leheküljest',
+		
+	/* product adding */
+	'type' => 'Tüüp',
+	'subtype' => 'Alamtüüp',
+	'manufactor' => 'Tootja',
+	'name' => 'Nimetus',
+	'year' => 'Aasta',
+	'alc' => 'Alkoholi sisaldus',
+	'origin' => 'Päritolumaa',
+	'selectOrigin' => 'Vali riik',
+	'user' => 'Kasutaja',
+	'cancel' => 'Tühista',
+	'save' => 'Salvesta',
+	'allProducts' => 'Kõik tooted',
+	'latestProducts' => 'Viimased tooted',
+	'noProducts' => 'Tooteid ei ole',
+	'productReviews' => 'Toote ülevaated',
+	'averageRating' => 'Keskmine hinnang',
+    'addReview' => 'Lisa ülevaade',
+	'topFresh' => 'Uued värsked',
+	'product' => 'Toode',
+	'color' => 'Välimus',
+	'smell' => 'Lõhn',
+	'taste' => 'Maitse',
+	'description' => 'Kirjeldus',
+	'rating' => 'Hinnang',
+	'notInList' => 'Ei leidnud toodet nimekirjast?',
+	'latestReviews' => 'Viimased ülevaated',
+	'allReviews' => 'Kõik ülevaated',
+	'picture' => 'Pilt (PNG)',
+		
+	
+	/* Product types and subtypes */
+	'beer' => 'Õlu',
+	'wine' => 'Vein',
+	'lager' => 'Lager',
+	'porter' => 'Porter',
+	'white' => 'Valge',
+	'red' => 'Punane',
+	'rose' => 'Roosa',
+	'ale' => 'Ale',
+	'mead' => 'Mõdu',
+	'sparkling' => 'Vahu',
+	'rootbeer' => 'Kali',
+	
+	'donate' => 'Anneta!',
+	'donateDetails' => '<p>Kui Sa hindad minu tehtud tööd, siis palun vajuta anneta nuppu! Äitähh!</p>',
+	/* 'aboutusDetails' => '<p>I am software architect/developer at Tieto Estonia AS who also enjoys a occasional beer or two.</p><p>I created this site to store all my findings in a newbish way.</p>', */
+	'help' => 'Abi',
+	'helpDetails' => '<p>&raquo; To enter review, go to <b>New Review</b> page and find the product You just tasted</p><p>&raquo; If You do not find the product from our list, go to <b>New Product</b> page and add it Yourself!',
+	'contactInfo' => 'Kontaktandmed',
+	'contactDetails' => 'Kui Sul on ettepanekuid, teateid vigadest,või muid märkusi, palun võtke minuga ühendust aadressil: markuskarileet@hotmail.com',	
+	'downloadDisclaimer' => 'Lehekülje stiil alla laetud',	
+	
+	/* technical messages */
+	'insertSuccess' => 'Lisamine edukas!',
+	'insertFail' => 'Lisamine ebaõnnestus',
+	'internalError' => 'Serveri viga mis on logitud ja millega tegeletakse, vabandame!',
+	'invalidInput' => 'Sisendandmed ei ole usaldusväärsed (ärge kasutage F5 nuppu)',
+	'notFound' => 'Otsitud objekti ei leitud andmebaasist!',
+			
+	/* adjectives */
+	'bitter' => 'Kibe',
+	'tasteless' => 'Maitsetu',
+	'sour' => 'Hapu',
+	'mellow' => 'Mahe',
+	'tasteful' => 'Maitsekas',
+	'hopy' => 'Humalane'
+);
